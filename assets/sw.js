4232d83b74768235c0e84afcee6152a66462e7ea
Barend Scholtus <barend.scholtus@gmail.com>
2024-10-07 22:27:56 +0700

    Basic app working

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './eframe_on_mac.js',
+  './eframe_on_mac_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
